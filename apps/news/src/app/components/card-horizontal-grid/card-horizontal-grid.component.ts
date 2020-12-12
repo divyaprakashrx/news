7982b22174d1bd13404b665cbc00c94655d52f17
princepriyank <mishrapriyank17@gmail.com>
2020-12-12 12:08:55 +0530

    added api to grid cards and made layout distinction

--- apps/news/src/app/components/card-horizontal-grid/card-horizontal-grid.component.ts
+++ apps/news/src/app/components/card-horizontal-grid/card-horizontal-grid.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { NewsInterface } from '../news.interface';
 @Component({
   selector: 'news-card-horizontal-grid',
   templateUrl: './card-horizontal-grid.component.html',
   styleUrls: ['./card-horizontal-grid.component.scss']
 })
 export class CardHorizontalGridComponent implements OnInit {
-
+  @Input() data: NewsInterface;
   constructor() { }
 
   ngOnInit(): void {
